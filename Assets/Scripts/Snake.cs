d3864be02c7d92c21aba3eca53faeadf42534137
Jubibani <strawberryloli3@gmail.com>
2025-07-10 19:09:05 +0800

    feat(snake): add snake segment

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,14 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
     private Rigidbody2D _rb;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _rb.WakeUp(); // This will ensure the Rigidbody2D is awake when the game starts
+    }
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+
     }
 
     private void Update()
@@ -21,10 +31,32 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         Vector2 newPos = new Vector2(
             Mathf.Round(transform.position.x) + _direction.x,
             Mathf.Round(transform.position.y) + _direction.y
         );
         _rb.MovePosition(newPos);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Food"))
+        {
+            Grow();
+        }
+
     }
 }
