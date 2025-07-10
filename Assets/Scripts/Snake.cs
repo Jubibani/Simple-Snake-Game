0829aa059f6ef1733f3bc09eb6cd9783d18c9311
Jubibani <strawberryloli3@gmail.com>
2025-07-10 18:41:53 +0800

    feat(food): add food game object

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -3,33 +3,28 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.WakeUp(); // This will ensure the Rigidbody2D is awake when the game starts
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            _direction = Vector2.up;
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            _direction = Vector2.down;
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            _direction = Vector2.left;
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _direction = Vector2.right;
-        }
+        if (Input.GetKeyDown(KeyCode.W) && _direction != Vector2.down) _direction = Vector2.up;
+        else if (Input.GetKeyDown(KeyCode.S) && _direction != Vector2.up) _direction = Vector2.down;
+        else if (Input.GetKeyDown(KeyCode.A) && _direction != Vector2.right) _direction = Vector2.left;
+        else if (Input.GetKeyDown(KeyCode.D) && _direction != Vector2.left) _direction = Vector2.right;
     }
 
     private void FixedUpdate()
     {
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x) + _direction.x,
-            Mathf.Round(this.transform.position.y) + _direction.y,
-            0.0f
+        Vector2 newPos = new Vector2(
+            Mathf.Round(transform.position.x) + _direction.x,
+            Mathf.Round(transform.position.y) + _direction.y
         );
+        _rb.MovePosition(newPos);
     }
 }
